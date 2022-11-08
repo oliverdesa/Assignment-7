0bddbe8b8cc676ee6a6728d682725c88a28fd0c1
Oliver De Sa <oliver.desa@mail.utoronto.ca>
2022-11-08 11:51:11 -0500

    fixed bootstrapping function, now outputs the correct boot data, not trying to create the CI function

--- A7.Utilities.R
+++ A7.Utilities.R
@@ -1,15 +1,16 @@
-source('Circle.Utilities.R')
+source('C:/Users/Oliver/MSC1090/assignment7/Circle.Utilities.R')
 library(boot)
 
 most.probable.value <- function(vector){
-  p.dist <- hist(vector, breaks = 41, freq = FALSE)
-  midPoint <- p.dist$mids[p.dist$density == max(p.dist$density)]
-  return(midPoint)
+  p.dist <- hist(vector, breaks = 41, freq = TRUE)
+  midPoint <- p.dist$mids[p.dist$counts == max(p.dist$counts)]
+  # midPoint <- p.dist$mids[abs((p.dist$density) - max(p.dist$density)) < 1e-10]
+  return(midPoint[1])
 }
 
 nonpara.bs <- function(vector, n){
-  my.mode <- function(vector, i) return(most.probable.value(vector[i]))
-  boot.data <- boot(data = vector, statistic = my.mode,
+  my.mpv <- function(vector, i) return(most.probable.value(vector[i]))
+  boot.data <- boot(data = vector, statistic = my.mpv,
                     R = n)
   return(boot.data)
   
@@ -17,6 +18,14 @@ nonpara.bs <- function(vector, n){
   
 }
 
-ang.diff.CI <- function(k, n, m){
-  sim.null.hypo(k, n)
+ang.diff.CI <- function(k, m, n){
+  all.ks <- seq(2, k)
+  get.CI <- function(k, m, n){
+    null.dists <- sim.null.hypo(k, m)
+    null.boot.data <- nonpara.bs(null.dists, n)
+    conf.intervals <- boot.ci(null.boot.data)
+    return(conf.intervals)
+  }
+  all.CIs <- sapply(all.ks, getCI)
+  
 }
